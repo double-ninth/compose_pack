3106f589baf4512b98f714fe68aa475cc3bd095a
guanhaicui <guanhaicui@xiao100.com>
2020-12-13 15:54:11 +0800

    composer pack test

---
+++ Index.php
@@ -0,0 +1,4 @@
+<?php
+require "vendor/autoload.php"; //引入自动加载文件
+$test = new Test\Test();
+$test->test();
---
+++ src/Test.php
@@ -0,0 +1,10 @@
+<?php
+namespace Test;
+
+class Test{
+    
+    function test()
+    {
+        echo "this is a package test";
+    }
+}
